86b6dab6483fdb120d2a0f3a1dc9bcc7698b8f48
v1r4m <choyoung7789@gmail.com>
2025-05-23 20:16:30 +0900

    rb change

--- Formula/launchd-plist-maker.rb
+++ Formula/launchd-plist-maker.rb
@@ -1,7 +1,7 @@
 class LaunchdPlistMaker < Formula
     desc "Ultimate launchd task installer for macOS"
-    homepage "https://github.com/v1r4m/homebrew-launchd-plist-maker"
-    url "https://github.com/v1r4m/homebrew-launchd-plist-maker/archive/refs/tags/v1.0.0.tar.gz"
+    homepage "https://github.com/v1r4m/launchd-plist-maker"
+    url "https://github.com/v1r4m/launchd-plist-maker/archive/refs/tags/v1.0.0.tar.gz"
     sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
     version "1.0.0"
   
